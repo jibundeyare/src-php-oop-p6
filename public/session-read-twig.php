34b1060768fc7bbcd1fdf995db4f357595abb547
Jibundeyare <contact@jibundeyare.com>
2020-06-05 15:11:40 +0200

    Ajout de code

---
+++ public/session-read-twig.php
@@ -0,0 +1,27 @@
+<?php
+
+// activation du système d'autoloading de Composer
+require __DIR__.'/../vendor/autoload.php';
+
+// instanciation du chargeur de templates
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../templates');
+
+// instanciation du moteur de template
+$twig = new \Twig\Environment($loader, [
+    // activation du mode debug
+    'debug' => true,
+    // activation du mode de variables strictes
+    'strict_variables' => true,
+]);
+
+// chargement de l'extension Twig_Extension_Debug
+$twig->addExtension(new \Twig\Extension\DebugExtension());
+
+session_start();
+
+// affichage du rendu d'un template
+echo $twig->render('session-read-twig.html.twig', [
+    // transmission de données au template
+    'session' => $_SESSION,
+]);
+
---
+++ public/session-write-twig.php
@@ -0,0 +1,35 @@
+<?php
+
+// activation du système d'autoloading de Composer
+require __DIR__.'/../vendor/autoload.php';
+
+// instanciation du chargeur de templates
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../templates');
+
+// instanciation du moteur de template
+$twig = new \Twig\Environment($loader, [
+    // activation du mode debug
+    'debug' => true,
+    // activation du mode de variables strictes
+    'strict_variables' => true,
+]);
+
+// chargement de l'extension Twig_Extension_Debug
+$twig->addExtension(new \Twig\Extension\DebugExtension());
+
+session_start();
+
+$_SESSION['foo'] = 'bar';
+
+$errors = [];
+
+if (!$errors) {
+    $_SESSION['login'] = 'toto';
+    $_SESSION['user_id'] = 123;
+}
+
+// affichage du rendu d'un template
+echo $twig->render('session-write-twig.html.twig', [
+    // transmission de données au template
+]);
+
